8d8881c586079aa5a936b17b6b220a3af7ccd291
ChristosLabrou <chris@labrou.gr>
2022-07-01 09:49:54 +0300

    Added top 5 tags functionality

--- WriteFullDetailsCSV.py
+++ WriteFullDetailsCSV.py
@@ -1,6 +1,7 @@
 import csv
 import time
 import GlobalVariables as GV
+import collections
 
 moviesList = [] #Column 0 is movieId, Column 1 is title, Column 2 is genres
 productionYear = []
@@ -46,7 +47,45 @@ with open(GV.CSVPATH+'sorted_rating.csv', 'r') as sortedcsv:
             j=0
             continue
         ratingslist.append(row)
-print("Finished reading. Took ", (time.time()-startTime), " seconds")
+print("Finished reading. Took %.2f seconds" %(time.time()-startTime))
+
+print("Reading sorted tags")
+startTime = time.time()
+totalTags = [] #Column 0 is movieId, Column 1 is tag
+with open(GV.CSVPATH+'sorted_tag.csv', 'r') as sortedcsv:
+    csv_reader = csv.reader(sortedcsv,delimiter=',')
+    for row in csv_reader:
+        totalTags.append([row[1], row[2]])
+print("Finished reading tags. Took %.2f seconds" %(time.time()-startTime))
+#-------------------------------
+#Separate tags by movie
+#-------------------------------
+
+currentMovieTags = [] #Holds all the tags of current movie
+tempList = []
+tempId = totalTags[0][0]
+mostCommonTags = []
+mostCommonTagsIDs = []
+
+for i in range(0, len(totalTags)):
+    if (tempId==totalTags[i][0]):
+        currentMovieTags.append(totalTags[i][1])
+    else:
+        counter = collections.Counter(currentMovieTags).most_common(5)
+        #counter is a list of tuples. Here we break the tuples to keep most common 5 tags only
+        for j in range(0, len(counter)):
+            tempList.append(counter[j][0])
+        mostCommonTags.append(tempList[:])
+        mostCommonTagsIDs.append(tempId)
+        tempId = totalTags[i][0]
+        tempList.clear()
+        currentMovieTags.clear()
+
+mergedList = [] #Combines ID & tags. Column 0 is movieId, Columns 1-5 are the 5 most common tags
+
+for i in range(0, len(mostCommonTags)):
+    mergedList.append([mostCommonTagsIDs[i]])
+    mergedList[i] = mergedList[i] + mostCommonTags[i]
 
 #-------------------------------
 #Calculate movie average rating
@@ -65,16 +104,16 @@ for x in range(1, len(ratingslist)):
         count = 1
         tempMovieId = ratingslist[x][1]
 
-#combinedList = []
-print("Started writing")
-startTime = time.time()
 with open(GV.CSVPATH+'fulldetails.csv', 'w') as export:
     writer = csv.writer(export)
     for i in range(0, len(moviesList)):
         writer.writerow([moviesList[i][0], moviesList[i][1], moviesList[i][2], productionYear[i]])
-print("Finished writing. Took %.2f seconds" % (time.time()-startTime))
 
 with open (GV.CSVPATH+'avgRating.csv', 'w') as export:
     writer = csv.writer(export)
     for i in range(0, len(movieAvgRating)):
         writer.writerow([movieAvgRating[i][0], movieAvgRating[i][1]])
+
+with open(GV.CSVPATH+'mostCommonTags.csv', 'w') as export:
+    writer = csv.writer(export)
+    writer.writerows(mergedList)
